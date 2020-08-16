5ad68ca416bc223822cae1f0747d056f65157061
IRCSS <sh.shahrabi@hotmail.com>
2020-08-16 21:03:51 +0200

    added the first screen space pass setup at the moment it only showcases the UV but is ready to be pluged in with other effects

---
+++ Assets/Scripts/ScreenSpaceMatrix.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class ScreenSpaceMatrix : MonoBehaviour {
+
+    // _____________________________________________
+    // Public
+    public  Texture font_texture;
+
+    // _____________________________________________
+    // Private 
+    private Camera        cam;
+    private Material      mat;
+    private CommandBuffer cb;
+
+	// Use this for initialization
+	void Start () {
+        cam = Camera.main;
+        if (!cam) Debug.LogError("Couldnt find the main camera, not such gameobject tagged as mainCamera");
+
+        mat = new Material(Shader.Find("Unlit/ScreenSpaceMatrixEffect"));
+        if (!mat) Debug.LogError("Couldnt find the shader ScreenSpaceMatrixEffect");
+
+        mat.SetTexture("_font_texture", font_texture);
+
+        cb = new CommandBuffer()
+        {
+            name = "ScreenSpaceMatrixPass",
+        };
+        cb.Blit(BuiltinRenderTextureType.None, BuiltinRenderTextureType.CameraTarget, mat);
+
+        cam.AddCommandBuffer(CameraEvent.AfterEverything, cb);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
