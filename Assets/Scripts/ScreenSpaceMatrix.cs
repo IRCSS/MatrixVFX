bbde4c3ea1ffce2a7ab3b0a2ed9bb1a1b613965a
IRCSS <sh.shahrabi@hotmail.com>
2020-08-18 23:16:03 +0100

    got the first version of the text function wokring

--- Assets/Scripts/ScreenSpaceMatrix.cs
+++ Assets/Scripts/ScreenSpaceMatrix.cs
@@ -7,37 +7,63 @@ public class ScreenSpaceMatrix : MonoBehaviour {
 
     // _____________________________________________
     // Public
-    public  Texture font_texture;
+    public  Texture       font_texture;
+    public  ComputeShader white_noise_generator;
+
 
     // _____________________________________________
     // Private 
     private Camera        cam;
     private Material      mat;
     private CommandBuffer cb;
+    private RenderTexture white_noise;
 
 	// Use this for initialization
 	void Start () {
         cam = Camera.main;
         if (!cam) Debug.LogError("Couldnt find the main camera, not such gameobject tagged as mainCamera");
 
+        // -----------------------------------------
+        white_noise = new RenderTexture(512, 512, 0)
+        {
+            name              = "white_noise",
+            enableRandomWrite = true,
+            useMipMap         = false,
+            filterMode        = FilterMode.Point,
+            wrapMode          = TextureWrapMode.Repeat
+            
+        };
+        white_noise.Create();
+        white_noise_generator.SetTexture(0, "_white_noise",    white_noise);
+
+        
+
+        // -----------------------------------------
+
         mat = new Material(Shader.Find("Unlit/ScreenSpaceMatrixEffect"));
         if (!mat) Debug.LogError("Couldnt find the shader ScreenSpaceMatrixEffect");
 
-        mat.SetTexture("_font_texture", font_texture);
-        mat.SetInt("_session_rand_seed", Random.Range(0, int.MaxValue));
+        mat.SetTexture("_font_texture",   font_texture);
+        mat.SetTexture("_white_noise",    white_noise);
+        mat.SetInt    ("_screen_width",   cam.pixelWidth);
+        mat.SetInt    ("_screen_height",  cam.pixelHeight);
+
+        // -----------------------------------------
+
         cb = new CommandBuffer()
         {
             name = "ScreenSpaceMatrixPass",
         };
-        cb.Blit(BuiltinRenderTextureType.None, BuiltinRenderTextureType.CameraTarget, mat);
 
+        cb.DispatchCompute(white_noise_generator, 0, 512 / 8, 512 / 8, 1);
+        cb.Blit(BuiltinRenderTextureType.None, BuiltinRenderTextureType.CameraTarget, mat);
+       // cb.Blit(white_noise, BuiltinRenderTextureType.CameraTarget);
         cam.AddCommandBuffer(CameraEvent.AfterEverything, cb);
-    }
+    } 
 	
 	// Update is called once per frame
 	void Update () {
-
-        mat.SetInt("_session_rand_seed", Random.Range(0, int.MaxValue));
+      white_noise_generator.SetInt("_session_rand_seed", Mathf.CeilToInt( Time.time *4.0f));
 
     }
 }
--- Assets/Shaders/Resources/ScreenSpaceMatrixEffect.shader
+++ Assets/Shaders/Resources/ScreenSpaceMatrixEffect.shader
@@ -25,9 +25,11 @@
 				float4 vertex : SV_POSITION;
 			};
 
-			uint      _session_rand_seed;
+			uint      _screen_width;
+			uint      _screen_height;
+
+			sampler2D _white_noise;
 			sampler2D _font_texture;
-			
 			// -----------------------------------
 
 			v2f vert (appdata v)
@@ -39,38 +41,29 @@
 				return o;
 			}
 
-			// --------------------------------------------------
-
-			// The below macro is used to get a random number which varies across different generations. 
-
-            #define rnd(seed, constant)  wang_rnd(seed +triple32(_session_rand_seed) * constant) 
-
-			uint triple32(uint x)
-			{
-				x ^= x >> 17;
-				x *= 0xed5ad4bbU;
-				x ^= x >> 11;
-				x *= 0xac4c1b51U;
-				x ^= x >> 15;
-				x *= 0x31848babU;
-				x ^= x >> 14;
-				return x;
-			}
-
-			float wang_rnd(uint seed)
-			{
-				uint rndint = triple32(seed);
-				return ((float)rndint) / float(0xFFFFFFFF);                                                       // 0xFFFFFFFF is max unsigned integer in hexa decimal
-			}
 			//---------------------------------------------------------
 			
+			float text(float2 coord)
+			{
+				float2 uv    = fmod(coord.xy, 16.) * 0.0625;
+				float2 block = floor(coord.xy/16.);
+				       uv    = uv * 0.8 + .1;
+			    float  rand  = tex2D(_white_noise, block.xy/float2(512.,512.));
+				
+				       rand = floor(rand*16.);
+				       uv   += float2(rand, rand);
+
+					   uv   *= 0.0625;
+					   uv.x  = -uv.x;
+			    return tex2D(_font_texture, uv).r;
+			}
+
 			fixed4 frag (v2f i) : SV_Target
 			{
 				// sample the texture
 				fixed4 col = tex2D(_font_texture, i.uv);
-			float rand = rnd(int(i.uv.x*1204. + 1024.*(i.uv.y*1024.)), 21);
-			col.xyz = float3(rand, rand, rand);
-				return col;
+			       col.xyz = float3(text(i.uv * float2(_screen_width, _screen_height)*0.6), 0., 0.);
+				return col.xxxx;
 			}
 			ENDCG
 		}
